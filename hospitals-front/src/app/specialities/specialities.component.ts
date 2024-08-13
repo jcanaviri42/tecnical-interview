66b6289c00a6a4ef9e81549960ccec5bb61cfcda
jcanaviri42 <jcanaviri42@gmail.com>
2024-08-13 01:09:07 -0400

    feat: basic setup and creating the menu

---
+++ hospitals-front/src/app/specialities/specialities.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specialities',
+  standalone: true,
+  imports: [],
+  templateUrl: './specialities.component.html',
+  styleUrl: './specialities.component.scss'
+})
+export class SpecialitiesComponent {
+
+}
